87ec673eeb98399d9161cb8dd4e956d9df7ab833
Leo <leotcort@gmail.com>
2024-01-18 15:08:34 -0300

    confg banco

---
+++ bancoDados/conexaoFilmes/conexaoFilmes.php
@@ -0,0 +1,8 @@
+<?php
+    $host = "localhost";
+    $user = "root";
+    $pass = "";
+    $bd = "sistemanetfilmes";
+
+    $conn = mysqli_connect($host, $user, $pass, $bd);
+?>
---
+++ bancoDados/conexaoNotas/conexaoNotas.php
@@ -0,0 +1,8 @@
+<?php
+    $host = "localhost";
+    $user = "root";
+    $pass = "";
+    $bd = "avaliacoesfilmes";
+
+    $conn = mysqli_connect($host, $user, $pass, $bd);
+?>
