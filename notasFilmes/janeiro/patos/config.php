720b05d29bb0f564dd492c32c64b400c861ec4c4
Leo <leotcort@gmail.com>
2024-01-22 10:40:16 -0300

    finalizando avaliacoes janeiro

---
+++ notasFilmes/janeiro/patos/config.php
@@ -0,0 +1,33 @@
+<?php
+session_start();
+
+include_once('../../../bancoDados/conexaoNotas/conexaoNotas.php');
+
+// Acessar o if quando e selecionado quando o usuario selecionar pelo menos 1 estrela
+if(!empty($_POST['estrela'])){ // se ele for diferente de vazio
+
+    // Receber os dados do formulario
+    $avaliacoes = filter_input(INPUT_POST, 'estrela', FILTER_DEFAULT);
+    $mensagem = filter_input(INPUT_POST, 'comentario', FILTER_DEFAULT);
+    
+    /* Criando a query para cadastrar no banco de dados */
+    $sql = "INSERT INTO patos(nota,comentario,data_comentario) VALUE('$avaliacoes','$mensagem',NOW())";
+    $result = mysqli_query($connNota, $sql);
+
+    if(!$result){
+        // Cria mensagem de erro 
+        $_SESSION['msg'] = "<p style='color: #f00;'>Erro: avaliação não cadastrada</p>";
+    }else{
+        header('Location: patos.php');
+    }
+
+}else{
+
+    // Cria mensagem de erro 
+    $_SESSION['msg'] = "<p style='color: #f00;'>Erro: tem que selecionar ao menos 1 estrela</p>";
+
+    // Redirecionando o usuario
+    header('Location: patos.php');
+}
+
+?>
---
+++ notasFilmes/janeiro/patos/notaMedia.php
@@ -0,0 +1,25 @@
+<?php
+include_once('../../../bancoDados/conexaoNotas/conexaoNotas.php');
+
+$sql = "SELECT nota FROM patos";
+$result = mysqli_query($connNota, $sql);
+
+$soma_notas = 0;
+$quant_registros = 0;
+$media = 0.0;
+
+while ($row = mysqli_fetch_array($result)) {
+    $soma_notas = $soma_notas + $row['nota'];
+    $quant_registros++;
+}
+
+if ($quant_registros > 0) {
+    $media = $soma_notas / $quant_registros;
+} else {
+    $media = 0;
+}
+
+// Exibir a média formatada
+echo number_format($media, 1);
+
+?>
